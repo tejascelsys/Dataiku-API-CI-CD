a1df6ed6805d256b111ce1135f2543a80e3b99c8
tejasprakash <tejas.prakash@celestialsys.com>
2025-11-05 17:27:53 +0530

    run packaging changed

--- 1_package_and_publish/run_packaging.py
+++ 1_package_and_publish/run_packaging.py
@@ -18,31 +18,64 @@ api_service = test_project.get_api_service(api_service_id)
 print("Found API Service to package {}".format(api_service))
 
 ####################
+# # Create and retrieve an API package
+
+# api_service.create_package(api_package_id)
+# print("New package created with name '{}'".format(api_package_id))
+# # Uncomment the next line to download and use the zip file (to upload it to an Artefact repository for example)
+# # api_service.download_package_to_file(api_package_id, api_package_id + ".zip")
+# version_as_stream = api_service.download_package_stream(api_package_id)
+# print("Stream handle retrieved on the package")
+
+# ####################
+# # Find the service as known by API Deployer
+
+# api_deployer = client.get_apideployer()
+# deployer_service = ""
+# for serv in api_deployer.list_services():
+#     if serv.id() == api_service_id:
+#         print("Found existing Deployer API service {}".format(api_service_id))
+#         deployer_service = serv
+# if deployer_service == "":
+#     print("Creating missing service {}".format(api_service_id))
+#     deployer_service = api_deployer.create_service(api_service_id)
+
+# ####################
+# # Import the new version
+
+# # new_version = deployer_service.import_version(api_package_id, version_as_stream)
+# new_version = deployer_service.import_version(version_as_stream)
+# print("New version published as '{}'".format(api_package_id))
+
+####################
 # Create and retrieve an API package
 
 api_service.create_package(api_package_id)
-print("New package created with name '{}'".format(api_package_id))
-# Uncomment the next line to download and use the zip file (to upload it to an Artefact repository for example)
-# api_service.download_package_to_file(api_package_id, api_package_id + ".zip")
-version_as_stream = api_service.download_package_stream(api_package_id)
-print("Stream handle retrieved on the package")
+print(f"New package created with name '{api_package_id}'")
+
+# Download the zip file locally so it can be uploaded to the remote deployer
+package_filename = f"{api_package_id}.zip"
+api_service.download_package_to_file(api_package_id, package_filename)
+print(f"Package downloaded locally as '{package_filename}'")
 
 ####################
 # Find the service as known by API Deployer
 
 api_deployer = client.get_apideployer()
-deployer_service = ""
+deployer_service = None
 for serv in api_deployer.list_services():
     if serv.id() == api_service_id:
-        print("Found existing Deployer API service {}".format(api_service_id))
+        print(f"Found existing Deployer API service {api_service_id}")
         deployer_service = serv
-if deployer_service == "":
-    print("Creating missing service {}".format(api_service_id))
+if deployer_service is None:
+    print(f"Creating missing service {api_service_id}")
     deployer_service = api_deployer.create_service(api_service_id)
 
 ####################
-# Import the new version
+# Import the new version to the remote deployer using the zip file
 
-# new_version = deployer_service.import_version(api_package_id, version_as_stream)
+with open(package_filename, 'rb') as f:
+    version_as_stream = f.read()
+
 new_version = deployer_service.import_version(version_as_stream)
-print("New version published as '{}'".format(api_package_id))
+print(f"New version published as '{api_package_id}'")
